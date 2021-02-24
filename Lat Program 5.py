9b37ae15f343b4aa94c8760939b33e2da2e591a0
DerwinAkbarNugroho <78077303+DerwinAkbarNugroho@users.noreply.github.com>
2021-02-24 17:28:40 +0700

    Delete Lat Program 5.py

--- Lat Program 5.py
+++
@@ -1,5 +0,0 @@
-angka = int(input('Masukkan angka: '))
-Batas = int(input('Masukkan batas perkalian + 1: ')) # Kalau misal ditulis 6, nanti perkaliannya sampai 5 saja
- 
-for i in range(1,Batas):
-    print(angka,'X', i,'=',angka*i)
